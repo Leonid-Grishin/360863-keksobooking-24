bad7a369638eb31016b16a79dab3cc5093b61e46
Leonid <alraun_work@mail.ru>
2021-09-25 22:45:45 +0300

    Первая проба

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ function getRandomIntInclusive(min, max) {
   max = (max < 0) ? 0 : Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(getRandomIntInclusive(10.5, -1));
+getRandomIntInclusive(10.5, -1);
 
 function getRandomFloatInclusive(min, max, float) {
   min = (min < 0) ? 0 : min;
   max = (max < 0) ? 0 : max;
   return (Math.random() * (max - min) + min).toFixed(float);
 }
-console.log(getRandomFloatInclusive(5, 1, 3));
+getRandomFloatInclusive(5, 1, 3);
 
