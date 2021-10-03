d987b1f71057eefcf32bd57af211db65b0ea7fac
Leonid <alraun_work@mail.ru>
2021-10-03 19:47:06 +0300

    итоговый вариант. Заменил создание пустых массивов на массивы конечной длины.

--- js/main.js
+++ js/main.js
@@ -19,59 +19,49 @@ const FEATURES_ALL = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'co
 const PHOTOS_ALL = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
 const SIMILAR_ADS_COUNT = 10;
 
-let numbers = []; // мы же потом этот массив изменяем, почему eslint говорит что нужно сделать const?
-for (let i = 1; i <= SIMILAR_ADS_COUNT; i++){
-  numbers.push(i);
+const NUMBERS = new Array(SIMILAR_ADS_COUNT).fill(0);
+for (let item = 0; item < NUMBERS.length; item++){
+  NUMBERS[item] = item+1;
 }
-let userNumbers = numbers.map((num) => (`0${num}`).slice(-2));  //тут мне eslint предложил исправить что-то, но я не понял как это работает, там где доллар. Эту часть я нашел на https://ru.stackoverflow.com/questions/772669/Форматирование-чисел-в-js-01-вместо-1
-let avatarsArray = [];
-userNumbers.forEach((num) => {
-  let avatarUrl = 'img/avatars/user' + num + '.png';
-  avatarsArray.push(avatarUrl);
-});
+const AVATARS_ARRAY = NUMBERS.map((element) => `img/avatars/user${(`0${element}`).slice(-2)}.png`);
 
-function getUniqueElement(arg) {
-  let oneAvatar = arg[0];
-  arg.shift();
-  return oneAvatar;
+function getUniqueElement(argument) {
+  const ONE_AVATAR = argument[0];
+  argument.shift();
+  return ONE_AVATAR;
 }
-
 const createAd = () => {
-  const AVATAR = getUniqueElement(avatarsArray);
-
-  const LOCATION_RAMDOM = {
+  const AVATAR = getUniqueElement(AVATARS_ARRAY);
+  const LOCATION_RANDOM = {
     lat: getRandomFloatInclusive(35.65000, 35.70000, 5),
     lng: getRandomFloatInclusive(139.70000, 139.80000, 5),
   };
+  const FEATURES_RANDOM = new Array(getRandomIntInclusive(1, FEATURES_ALL.length))
+    .fill(0)
+    .map((item, index) => FEATURES_ALL[index]);
+  const PHOTOS_RANDOM = new Array(getRandomIntInclusive(1, PHOTOS_ALL.length))
+    .fill(0)
+    .map((item, index) => PHOTOS_ALL[index]);
 
-  const featuresRandom = [];
-  for (let i = 0; i <= getRandomIntInclusive(0, FEATURES_ALL.length-1); i++) {
-    featuresRandom.push(FEATURES_ALL[i]);
-  }
-
-  let photosRandom = [];
-  for (let i = 0; i <= getRandomIntInclusive(0, PHOTOS_ALL.length-1); i++) {
-    photosRandom.push(PHOTOS_ALL[i]);
-  }
   return {
     author: AVATAR,
     offer: {
       title: 'Место для чила',
-      address: LOCATION_RAMDOM, //не понимаю как записать адрес с использованием маски
-      price: getRandomIntInclusive(0, 100), // как сделать до бесконечности?
+      address: `${LOCATION_RANDOM.lat}, ${LOCATION_RANDOM.lng}`,
+      price: getRandomIntInclusive(0, 100),
       type: TYPES[getRandomIntInclusive(0, TYPES.length-1)],
-      guests: getRandomIntInclusive(0, 100), // как сделать до бесконечности?
+      guests: getRandomIntInclusive(0, 100),
       checkin: CHECKINS[getRandomIntInclusive(0, CHECKINS.length-1)],
       checkout: CHECKOUTS[getRandomIntInclusive(0, CHECKOUTS.length-1)],
-      features: featuresRandom,
+      features: FEATURES_RANDOM,
       description: 'место для уютного отдыха',
-      photos: photosRandom,
+      photos: PHOTOS_RANDOM,
     },
-    location: LOCATION_RAMDOM,
+    location: LOCATION_RANDOM,
   };
 };
 
 const SIMILAR_ADS = Array.from({length: SIMILAR_ADS_COUNT}, createAd);
-
