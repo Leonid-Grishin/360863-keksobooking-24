8e908d76b208cfd5916ae23d2a11b3b2cba1c391
Leonid <alraun_work@mail.ru>
2021-10-07 14:00:15 +0300

    Сделал задание

--- js/util.js
+++ js/util.js
@@ -1,4 +1,20 @@
-const rrr = 1;
+function getRandomIntInclusive(min, max) {
+  min = (min < 0) ? 0 : Math.ceil(min);
+  max = (max < 0) ? 0 : Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-rrr;
-export {rrr};
+function getRandomFloatInclusive(min, max, float) {
+  min = (min < 0) ? 0 : min;
+  max = (max < 0) ? 0 : max;
+  return (Math.random() * (max - min) + min).toFixed(float);
+}
+
+function getUniqueElement(argument) {
+  const ONE_AVATAR = argument[0];
+  argument.shift();
+  return ONE_AVATAR;
+}
+
+export {getRandomIntInclusive, getRandomFloatInclusive, getUniqueElement};
+
